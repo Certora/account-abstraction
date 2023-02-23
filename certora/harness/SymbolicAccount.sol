dc55165cf0089a635aee438ef9085263b7e8a233
Yura Sherman <yurasherman+github@gmail.com>
2023-02-23 19:09:53 +0530

    add a simple execute()

--- certora/harness/SymbolicAccount.sol
+++ certora/harness/SymbolicAccount.sol
@@ -10,6 +10,15 @@ contract SymbolicAccount  {
 
     // unconstraint values - can be any value
     mapping(bytes32 => ValidationData) public validationReturnValue;
+    
+    uint count;
+    mapping(uint => bytes) public calldatas;
+
+
+    constructor() {
+        count = 0;
+    }
+
     function validateUserOp(UserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
     external  returns (uint256 validationData)
     {
@@ -18,6 +27,17 @@ contract SymbolicAccount  {
                                                     validationReturnValue[encode].validAfter,
                                                     validationReturnValue[encode].validUntil);
 
+    }
+
+
+    // writing as a fallback function requires assembly, which will take some time for
+    // me to learn writing. 
+    // let's start with a simple implementation as `execute`
+
+    function execute(address target, uint256 value, bytes calldata data) external {
+        // _requireFromEntryPointOrOwner();
+        calldatas[count] = data;
+        count++;
     }
 
     //todo - in fallback mark which userOp has been called
