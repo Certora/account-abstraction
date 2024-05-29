4281615897a1261726179c91392a9dd76446bf12
Jochen Hoenicke <hoenicke@gmail.com>
2024-05-29 19:04:51 +0200

    change to conf file

--- certora/scripts/verifyEntryPoint.sh
+++
@@ -1,9 +0,0 @@
-certoraRun.py \
-  contracts/core/EntryPoint.sol certora/harness/SymbolicAccount.sol \
-  --verify EntryPoint:certora/specs/entryPoint.spec \
- --solc solc8.12 \
- --staging master \
- --optimistic_loop --optimistic_hashing --settings -optimisticFallback=true,-depth=25,-t=400,-dontStopAtFirstSplitTimeout=true \
- --link EntryPoint:constantSender=SymbolicAccount \
- --loop_iter 1 \
- --msg "entrypoint $1"
